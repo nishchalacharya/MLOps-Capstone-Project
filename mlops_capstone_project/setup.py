53e69ff11443211ef0265903b90bfe623ed75fb1
Nishchal Sharma <nishchalsharma456@gmail.com>
2026-04-02 10:35:38 +0545

    skeleton developed by cookiecutter-datascience format

---
+++ mlops_capstone_project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a project that demonstrates every aspects used for machine learning model development to evaluation/monitoring for mlops operation',
+    author='Nishchal Sharma',
+    license='MIT',
+)
